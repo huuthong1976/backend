a68597a6462c26033aecb238777307c051c11763
huuthong1976 <huuthong27061976@gmail.com>
2025-09-11 03:42:21 +0700

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -3,12 +3,20 @@
 
 const fs = require('fs');
 const path = require('path');
-const { DataTypes } = require('sequelize,Sequelize');
-const sequelize = require('../../db'); 
+const Sequelize = require('sequelize'); 
+const process = require('process');
 const basename = path.basename(__filename);
+const sequelize = require('../../db'); 
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 
 const db = {};
-
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 fs.readdirSync(__dirname)
   .filter((file) =>
     file.indexOf('.') !== 0 &&
