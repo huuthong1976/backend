3f5d634e548aa96899a2758da7c0b7e1b76bb33e
huuthong1976 <huuthong27061976@gmail.com>
2025-09-11 03:26:54 +0700

    Update Position.js

--- src/models/Position.js
+++ src/models/Position.js
@@ -1,16 +1,23 @@
 'use strict';
-
 module.exports = (sequelize, DataTypes) => {
- 
   const Position = sequelize.define('Position', {
-   
-    title: {
-      type: DataTypes.STRING,
+    // Khóa chính, tự động tăng
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
       allowNull: false
     },
-    description: {
-      type: DataTypes.STRING
+    // Tên chức vụ
+    position_name: {
+      type: DataTypes.STRING,
+      allowNull: false 
     }
+  }, {
+    
+    timestamps: false,
+   
+    tableName: 'positions'
   });
 
   return Position;
