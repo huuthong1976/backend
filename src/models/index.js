b3d2bf386d01e27d1d612fdd84e1baeb74cd98f6
huuthong1976 <huuthong27061976@gmail.com>
2025-09-11 03:45:54 +0700

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const Sequelize = require('sequelize'); 
 const process = require('process');
 const basename = path.basename(__filename);
-const sequelize = require('../../db'); 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 
