503daf5fd80655c6c377ddacab501dd2475c426d
huuthong1976 <huuthong27061976@gmail.com>
2025-09-10 21:19:23 +0700

    Update Position.js

--- src/models/Position.js
+++ src/models/Position.js
@@ -1,16 +1,20 @@
 'use strict';
-const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Position extends Model {
-    static associate(models) {}
-  }
-  Position.init({
-    position_name: { type: DataTypes.STRING, allowNull: false }
-  }, {
-    sequelize,
-    modelName: 'Position',
-    tableName: 'positions',
-    timestamps: false,
-  });
+  const Position = sequelize.define(
+    'Position',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      code: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+      name: { type: DataTypes.STRING(255), allowNull: false }
+    },
+    {
+      tableName: 'positions',
+      timestamps: true
+    }
+  );
+
+  // Position.associate = (models) => { ... };
+
   return Position;
 };
